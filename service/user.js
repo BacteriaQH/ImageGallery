23c73c3db2c841980ecb6b8001a9a3dd024e5122
BacteriaQH <hoangphuc2k1vp@gmail.com>
2023-03-11 04:02:27 +0700

    add user service

---
+++ service/user.js
@@ -0,0 +1,54 @@
+const db = require('../models/index');
+const createUser = async (user) => {
+    try {
+        const userS = await db.User.create(user);
+        return userS;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+
+const findUserById = async (id) => {
+    try {
+        const userS = await db.User.findOne({ where: { id: id } });
+        return userS;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+const findUserByName = async (username) => {
+    try {
+        const userS = await db.User.findOne({ where: { username: username } });
+        return userS;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+const getAllUser = async () => {
+    try {
+        const userS = await db.User.findAll();
+        return userS;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+const deleteUser = async (id) => {
+    try {
+        const userS = await db.User.destroy({ where: { id: id } });
+        return userS;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+};
+module.exports = {
+    createUser,
+    findUserById,
+    findUserByName,
+    getAllUser,
+    deleteUser,
+};
